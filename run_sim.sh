12777ea28f0c5511ad9a196b8aad4bffaf039759
JAIDHEER SIRIGINEEDI <72718607+JAIDHEER007@users.noreply.github.com>
2024-12-10 17:16:18 -0600

    Replaceed with 1MB size

--- run_sim.sh
+++ run_sim.sh
@@ -5,25 +5,25 @@ else
     sed -i 's/\r$//' run_sim.sh
 fi
 if [ $1 = "1KB_64B" ]; then
-    python3 simulator.py $1 1024 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "4MB_4B" ]; then
-    python3 simulator.py $1 4194304 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "32MB_4B" ]; then
-    python3 simulator.py $1 4194304 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "bw_mem.traces.txt" ]; then
-    python3 simulator.py $1 4194304 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "ls.trace.txt" ]; then 
-    python3 simulator.py $1 32768 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "gcc.trace.txt" ]; then
-    python3 simulator.py $1 32768 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "naive_dgemm.trace.txt" ]; then
-    python3 simulator.py $1 262144 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "naive_dgemm_full.trace.txt" ]; then
-    python3 simulator.py $1 262144 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "openblas_dgemm.trace.txt" ]; then
-    python3 simulator.py $1 262144 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 elif [ $1 = "openblas_dgemm_full.trace.txt" ]; then
-    python3 simulator.py $1 262144 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 else
-    python3 simulator.py $1 1024 64 16 LRU
+    python3 simulator.py $1 1048576 64 16 LRU
 fi
